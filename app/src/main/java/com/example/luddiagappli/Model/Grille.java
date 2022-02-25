bfaf9b2bc8313189cd8ed91f493e9aea2fd91598
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-02-25 16:11:02 +0100

    ADD Grille.java UPD MainActivity.java
    Ajout de la classe Grille et exemple de récupération de donnée dans Main activity

---
+++ app/src/main/java/com/example/luddiagappli/Model/Grille.java
@@ -0,0 +1,17 @@
+package com.example.luddiagappli.Model;
+
+import java.util.Date;
+
+public class Grille {
+
+    private int id;
+    private boolean rempli;
+    private String type;
+    private Date date_deb;
+    private Date date_fin;
+    //private Joueur joueur;
+    //private List<Jeux> listJeux;
+
+
+
+}
--- app/src/main/java/com/example/luddiagappli/View/MainActivity.java
+++ app/src/main/java/com/example/luddiagappli/View/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.luddiagappli.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+
 import com.example.luddiagappli.Model.database;
 import com.example.luddiagappli.R;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //code a inclure
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+        }
+        //Example de récupération de donnée
         database db = new database();
+        try {
+            Statement st = db.getExtraConnection().createStatement();
+            String sql = "SELECT * FROM user_flip";
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                String username = rs.getString("nom_user");
+                Log.d("DB" , username);
+            }
+        } catch (SQLException e) {
+            Log.d("DB", "Error getting querry");
+            e.printStackTrace();
+        }
     }
+
+
 }
