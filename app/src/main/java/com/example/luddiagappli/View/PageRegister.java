e72a105ae7eb94cab1f066825a42dd0f4b7515af
ClementLegros <clement.legros.91220@gmail.com>
2022-03-14 22:20:18 +0100

    Ajout des pages

---
+++ app/src/main/java/com/example/luddiagappli/View/PageRegister.java
@@ -0,0 +1,16 @@
+package com.example.luddiagappli.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.luddiagappli.R;
+
+public class PageRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_register);
+    }
+}
