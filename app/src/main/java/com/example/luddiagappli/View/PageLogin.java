ae03e0a3421470daab4dd25e810ca6bc9acebd6b
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-04-07 17:05:59 +0200

    Update PageLogin.java
    add TestCredientials function

--- app/src/main/java/com/example/luddiagappli/View/PageLogin.java
+++ app/src/main/java/com/example/luddiagappli/View/PageLogin.java
@@ -3,8 +3,14 @@ package com.example.luddiagappli.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.luddiagappli.Model.database;
 import com.example.luddiagappli.R;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PageLogin extends AppCompatActivity {
 
@@ -12,5 +18,34 @@ public class PageLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_login);
+    }
+
+
+
+
+    public boolean testCredentials(String username, String password)
+    {
+        Integer nbConnection = 0;
+        database db = new database();
+        try {
+            Statement st = db.getExtraConnection().createStatement();
+            String sql = "SELECT count(*) as nbConnection FROM user_flip where nom_user= '"+username+"' and mdp_user'"+password+"'";
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next())
+            {
+                nbConnection = rs.getInt("nbConnection");
+            }
+        } catch (SQLException e) {
+            Log.d("DB", "Error getting querry");
+            e.printStackTrace();
+        }
+
+        if(nbConnection == 1)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
