8a6978177ec7cbd5f61bb481e88f2474fab9053a
tplulu <59656493+tplulu@users.noreply.github.com>
2022-06-09 11:31:05 +0200

    création des pages pour choisir les flips

---
+++ app/src/main/java/com/example/luddiagappli/flip_createur.java
@@ -0,0 +1,14 @@
+package com.example.luddiagappli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class flip_createur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flip_createur);
+    }
+}
