7dc7a7ab86272467dfc8726e51ea4146bbe0be5e
Marinator89 <43178189+Leadersheepy@users.noreply.github.com>
2022-06-10 11:25:15 +0200

    Lien page login

--- app/src/main/java/com/example/luddiagappli/View/PageLogin.java
+++ app/src/main/java/com/example/luddiagappli/View/PageLogin.java
@@ -2,11 +2,14 @@ package com.example.luddiagappli.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.luddiagappli.Model.database;
 import com.example.luddiagappli.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,14 +17,21 @@ import java.sql.Statement;
 
 public class PageLogin extends AppCompatActivity {
 
+    private FloatingActionButton boutonArriereMain;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_login);
+
+        boutonArriereMain = (FloatingActionButton) findViewById(R.id.boutonArriereMain);
+        boutonArriereMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+                openMain();
+            }
+        });
     }
-
-
-
 
     public boolean testCredentials(String username, String password)
     {
@@ -48,4 +58,11 @@ public class PageLogin extends AppCompatActivity {
             return false;
         }
     }
+
+
+    public void openMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
